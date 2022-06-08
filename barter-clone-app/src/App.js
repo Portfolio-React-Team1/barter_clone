22b9c9cdcbd124d24f892aa76f6cf477d31185b1
Elameen123 <69402639+Elameen123@users.noreply.github.com>
2022-06-08 16:13:57 +0100

    Create Account Page without authentification

--- barter-clone-app/src/App.js
+++ barter-clone-app/src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import Signup from './pages/Create_account/Signup';
 
 function App() {
   return (
     <div className="App">
-     
+     <Signup></Signup>
     </div>
   );
 }
